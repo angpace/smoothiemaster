f886acc9196b9f205ef493a397cad3f0fc1e1c62
Keshav Bhati <keshavbhati2000@gmail.com>
2022-10-03 19:44:14 -0400

    render imgs but they are very big

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const fruitBasket = document.querySelector("#fruitList")
+
+    fetch("http://localhost:3000/Fruits")
+    .then(data => data.json())
+    .then(res => res.forEach(element => {
+        fruitRender(element)
+    }))
+
+    //function to render the fruits in the ingredients section
+    function fruitRender(fruit){
+        let newFruit = document.createElement('div')
+        let fruitImg = document.createElement('img')
+        let fruitName = document.createElement('p')
+
+        fruitImg.src = fruit.image
+        fruitName.textContent = fruit.name
+        
+        newFruit.className = "fruits"
+        newFruit.id = fruit.id
+
+        newFruit.append(fruitImg)
+        newFruit.append(fruitName)
+        fruitBasket.append(newFruit)
+    }
 
 })

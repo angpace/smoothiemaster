237f4e420e2b1184e06e2d004e9cb3346d52c759
Keshav Bhati <keshavbhati2000@gmail.com>
2022-10-04 16:53:11 -0400

    able to access nutrional values

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
     const fruitBasket = document.querySelector("#basket")
-    console.log(fruitBasket)
+    //console.log(fruitBasket)
 
     fetch("http://localhost:3000/Fruits")
         .then(res => res.json())
         .then(data => fruitRender(data))
 
 
-//function to render the fruits in the ingredients section
+    //function to render the fruits in the ingredients section
     function fruitRender(data) {
     data.forEach(fruit => {
         const newFruitImage = document.createElement('img')
         newFruitImage.src = fruit.image
         newFruitImage.className = "fruit"
-        
+        newFruitImage.id = fruit.name
+        const fruitNutrition = document.createElement('p')
+        for(let key in fruit.nutritions){
+            fruitNutrition.textContent += `${fruit.nutritions[key]} `
+        }
+        console.log(fruitNutrition)
+
+        newFruitImage.appendChild(fruitNutrition)
+
+        //adding the eventlistener for drag n drop
         newFruitImage.addEventListener('dragstart', drag)
         fruitBasket.addEventListener('drop', drop)
         fruitBasket.addEventListener('dragover', allowDrop)
         
-
+        
         fruitBasket.append(newFruitImage)
         
     });
-
     }
     const blendDiv = document.querySelector(".card")
     const blender = document.getElementById("blender")
 
+    //adding the event listener to the blender
     blender.addEventListener('drop', drop)
     blender.addEventListener('dragover', allowDrop)
     
@@ -56,8 +65,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
       
     function drag(e) {
-        console.log(e.target)
-        e.dataTransfer.setData("text", e.target.nutritions);
+        console.log(e.target.id)
+        e.dataTransfer.setData("text", e.target.id);
     }
       
     function drop(e) {
